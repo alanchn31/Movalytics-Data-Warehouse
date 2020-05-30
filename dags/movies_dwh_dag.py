c7ab03be05981f22bb8ddc2b2fa3cfa218249051
alanchn31 <alanchn31@gmail.com>
2020-05-30 21:32:06 +0800

    Refactor Bash Code in spark submit jobs
    For spark submit jobs, loading stage tables to redshift, simplify the
    code by refactoring into a bash script

--- dags/movies_dwh_dag.py
+++ dags/movies_dwh_dag.py
@@ -7,16 +7,20 @@ from airflow.models import Variable
 from datetime import datetime, timedelta
 import os
 
+# Define configured variables to connect to AWS S3 and Redshift
 movie_s3_config = Variable.get("movie_s3_config", deserialize_json=True)
-redshift_conn_string = Variable.get("redshift_conn_string")
-db_user = Variable.get("redshift_db_user")
-db_pass = Variable.get("redshift_db_pass")
-s3_bucket = movie_s3_config["s3Bucket"]
-s3_key = movie_s3_config["s3Key"]
-aws_key =  movie_s3_config["awsKey"]
-aws_secret_key = movie_s3_config["awsSecretKey"]
 
-## Define the DAG object
+# Parameters that are reused when submitting spark job to load staging tables
+params = {'aws_key': movie_s3_config["awsKey"],
+          'aws_secret_key': movie_s3_config["awsSecretKey"],
+          'db_user': Variable.get("redshift_db_user"),
+          'db_pass': Variable.get("redshift_db_pass"),
+          'redshift_conn_string': Variable.get("redshift_conn_string"),
+          's3_bucket': movie_s3_config["s3Bucket"],
+          's3_key': movie_s3_config["s3Key"]  
+         }
+
+# Default settings for DAG
 default_args = {
     'owner': 'Alan',
     'depends_on_past': False,
@@ -24,48 +28,41 @@ default_args = {
     'retries': 5,
     'retry_delay': timedelta(minutes=1),
 }
+
+## Define the DAG object
 with DAG(dag_id='sparkify_movie_dwh_dag', default_args=default_args,
          description='Load and transform data in Redshift \
                       Data Warehouse with Airflow',
          schedule_interval='@daily') as dag:
     
     start_operator = DummyOperator(task_id='begin-execution', dag=dag)
-    create_tables = PostgresOperator(task_id='create-tables', postgres_conn_id="redshift",
-                                    sql="sql_scripts/create_tables.sql", dag=dag)
-    
-    load_staging_ratings = BashOperator(task_id='load-staging-ratings',
-                                        bash_command= 'spark-submit ' + \
-                                        '--driver-class-path $SPARK_HOME/jars/RedshiftJDBC42-no-awssdk-1.2.43.1067.jar ' + \
-                                        '--jars $SPARK_HOME/jars/RedshiftJDBC42-no-awssdk-1.2.43.1067.jar ' + \
-                                        os.getcwd() + '/dags/python_scripts/' + \
-                                        'load_staging_ratings.py ' + s3_bucket + ' ' + s3_key + \
-                                        ' ' + aws_key + ' ' + aws_secret_key + ' ' + redshift_conn_string + \
-                                        ' ' + db_user + ' ' + db_pass + ' ' + \
-                                        '--conf "fs.s3a.multipart.size=104857600"' 
-                                        , dag=dag)
-    
-    load_staging_movies = BashOperator(task_id='load-staging-movies',
-                                        bash_command= 'spark-submit ' + \
-                                        '--driver-class-path $SPARK_HOME/jars/RedshiftJDBC42-no-awssdk-1.2.43.1067.jar ' + \
-                                        '--jars $SPARK_HOME/jars/RedshiftJDBC42-no-awssdk-1.2.43.1067.jar ' + \
-                                        os.getcwd() + '/dags/python_scripts/' + \
-                                        'load_staging_movies.py ' + s3_bucket + ' ' + s3_key + \
-                                        ' ' + aws_key + ' ' + aws_secret_key + ' ' + redshift_conn_string + \
-                                        ' ' + db_user + ' ' + db_pass + ' ' + \
-                                        '--conf "fs.s3a.multipart.size=104857600"' 
-                                        , dag=dag)
 
-    load_staging_cpi = BashOperator(task_id='load-staging-cpi',
-                                        bash_command= 'spark-submit ' + \
-                                        '--driver-class-path $SPARK_HOME/jars/RedshiftJDBC42-no-awssdk-1.2.43.1067.jar ' + \
-                                        '--jars $SPARK_HOME/jars/RedshiftJDBC42-no-awssdk-1.2.43.1067.jar ' + \
-                                        os.getcwd() + '/dags/python_scripts/' + \
-                                        'load_staging_cpi.py ' + s3_bucket + ' ' + s3_key + \
-                                        ' ' + aws_key + ' ' + aws_secret_key + ' ' + redshift_conn_string + \
-                                        ' ' + db_user + ' ' + db_pass + ' ' + \
-                                        '--conf "fs.s3a.multipart.size=104857600"' 
-                                        , dag=dag)
+    # Create tables in movies schema
+    create_tables = PostgresOperator(task_id='create-tables', postgres_conn_id="redshift",
+                                     sql="sql_scripts/create_tables.sql", dag=dag)
     
+    # Load stage_ratings data table
+    params['python_script'] = 'load_staging_ratings.py'
+    load_staging_ratings = BashOperator(task_id='load-staging-ratings',
+                                        bash_command= './bash_scripts/load_staging_table.sh',
+                                        params=params,
+                                        dag=dag)
+    
+    # Load stage_movies data table
+    params['python_script'] = 'load_staging_movies.py'
+    load_staging_movies = BashOperator(task_id='load-staging-movies',
+                                       bash_command= './bash_scripts/load_staging_table.sh',
+                                       params=params,
+                                       dag=dag)
+
+    # Load stage_cpi data table
+    params['python_script'] = 'load_staging_cpi.py'
+    load_staging_cpi = BashOperator(task_id='load-staging-cpi',
+                                    bash_command= './bash_scripts/load_staging_table.sh',
+                                    params=params,
+                                    dag=dag)
+    
+    # Run upsert on tables and delete staging tables
     upsert_ratings = PostgresOperator(task_id='upsert-ratings-table', postgres_conn_id="redshift",
                                     sql="sql_scripts/upsert_ratings.sql", dag=dag)
 
@@ -73,27 +70,17 @@ with DAG(dag_id='sparkify_movie_dwh_dag', default_args=default_args,
                                      sql="sql_scripts/upsert_movies.sql", dag=dag)
 
     upsert_cpi = PostgresOperator(task_id='upsert-staging-cpi', postgres_conn_id="redshift",
-                                  sql='sql_scripts/upsert_cpi.sql',
-                                  params={
-                                    'schema': 'movies',
-                                    'table': 'stage_cpi',
-                                    's3_bucket': s3_bucket,
-                                    's3_key': s3_key,
-                                    'file_name': 'consumer_price_index.csv',
-                                    'access_key':  aws_key,
-                                    'secret_key': aws_secret_key
-                                  }, dag=dag)
+                                  sql='sql_scripts/upsert_cpi.sql', dag=dag)
     
+    # Check for quality issues in ingested data
+    tables = ["movies.movies", "movies.ratings", "movies.movie_genre",
+              "movies.genre", "movies.date", "movies.cpi"]
     check_data_quality = DataQualityOperator(task_id='run_data_quality_checks',
                                             redshift_conn_id="redshift",
-                                            table_names=["movies.movies",
-                                                         "movies.ratings",
-                                                         "movies.movie_genre",
-                                                         "movies.genre",
-                                                         "movies.date",
-                                                         "movies.cpi"],
+                                            table_names=tables,
                                             dag=dag)
 
+    # Define data pipeline DAG structure
     start_operator >> create_tables
     create_tables >> [load_staging_ratings, load_staging_movies, load_staging_cpi]
     load_staging_ratings >> upsert_ratings
