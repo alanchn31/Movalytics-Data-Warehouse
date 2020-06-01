091fb04bee3abca645d16ff5ba67406fa9ee643d
alanchn31 <alanchn31@gmail.com>
2020-06-01 21:15:10 +0800

    Edit Bash script to remove '

--- dags/bash_scripts/load_staging_table.sh
+++ dags/bash_scripts/load_staging_table.sh
@@ -2,4 +2,4 @@ spark-submit --driver-class-path $SPARK_HOME/jars/RedshiftJDBC42-no-awssdk-1.2.4
 --jars $SPARK_HOME/jars/RedshiftJDBC42-no-awssdk-1.2.43.1067.jar \
 $PWD/dags/python_scripts/{{params.python_script}} {{ params.s3_bucket }} {{ params.s3_key }} \
 {{ params.aws_key }} {{ params.aws_secret_key }} {{ params.redshift_conn_string }} \
-{{ params.db_user }} {{params.db_pass}} --conf "fs.s3a.multipart.size=104857600"'
+{{ params.db_user }} {{params.db_pass}} --conf "fs.s3a.multipart.size=104857600"
